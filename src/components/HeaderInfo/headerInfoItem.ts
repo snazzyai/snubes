bce8a862b0be1b7501f0735c34a289e6c8850a11
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2022-02-03 17:02:50 +0100

    created ts items for page

---
+++ src/components/HeaderInfo/headerInfoItem.ts
@@ -0,0 +1,25 @@
+interface HeaderInfoItem {
+  numbers: string
+  title: string
+}
+
+const headerInfoItem: HeaderInfoItem[] = [
+  {
+    numbers: '500+',
+    title: 'Tenders',
+  },
+  {
+    numbers: '200+',
+    title: 'Callcenter',
+  },
+  {
+    numbers: '375.000',
+    title: 'Agents available',
+  },
+  {
+    numbers: '85%',
+    title: 'Faster sourcing',
+  },
+];
+
+export default headerInfoItem;
