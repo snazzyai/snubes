5637da3846e36aa446dc4a1436f93304b6e41af9
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2022-02-04 20:37:51 +0100

    created data items

---
+++ src/components/Testimonial/TestimonialItems.ts
@@ -0,0 +1,33 @@
+interface TestimonialInfoItem {
+  imgUrl: string
+  quote: string
+  quoter: string
+  position: string
+  imgAlt: string
+}
+
+const testimonialItems: TestimonialInfoItem[] = [
+  {
+    imgUrl: '/src/assets/images/talixo-logo-black-copy-5-2.png',
+    quote: '"Finding a suitable outsourcing service provider through Snubes was very easy and our personal consultant helped us every step of the way. After a short time we had good offers on the table. An on-site visit confirmed the positive impression and we have now found a matching partner. Thank you for your great support, which has helped us a lot in the selection process."',
+    quoter: 'Jan Brenneke',
+    position: 'VP Operations',
+    imgAlt: 'talixo logo',
+  },
+  {
+    imgUrl: '/src/assets/images/talixo-logo-black-copy-5-2.png',
+    quote: '"Finding a suitable outsourcing service provider through Snubes was very easy and our personal consultant helped us every step of the way. After a short time we had good offers on the table. An on-site visit confirmed the positive impression and we have now found a matching partner. Thank you for your great support, which has helped us a lot in the selection process."',
+    quoter: 'Jan Brenneke',
+    position: 'VP Admin',
+    imgAlt: 'talixo logo',
+  },
+  {
+    imgUrl: '/src/assets/images/talixo-logo-black-copy-5-2.png',
+    quote: '"Finding a suitable outsourcing service provider through Snubes was very easy and our personal consultant helped us every step of the way. After a short time we had good offers on the table. An on-site visit confirmed the positive impression and we have now found a matching partner. Thank you for your great support, which has helped us a lot in the selection process."',
+    quoter: 'Jan Brenneke',
+    position: 'VP Logistics',
+    imgAlt: 'talixo logo',
+  },
+];
+
+export default testimonialItems;
