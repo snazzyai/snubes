61c03acfa6b253679ed524f5c5366aef34c18fcb
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2022-02-04 20:38:38 +0100

    added component

--- src/components/Layout/Body/Body.tsx
+++ src/components/Layout/Body/Body.tsx
@@ -1,12 +1,14 @@
 import Head from '../../Head/Head';
 import Offerings from '../../Offerings/Offerings';
 import WhySnubes from '../../WhySnubes/WhySnubes';
+import Testimonial from '../../Testimonial/Testimonial';
 
 const Body: React.FC = () => (
   <div>
     <Head />
     <Offerings />
     <WhySnubes />
+    <Testimonial />
   </div>
 );
 
