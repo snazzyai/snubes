a3a2ccdb9d3f64eb9b0516f01568be8a36aa0963
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2022-02-04 21:43:01 +0100

    created data items for component

---
+++ src/components/Partners/PartnersItems.tsx
@@ -0,0 +1,29 @@
+interface PartnersInfoItems {
+  imgUrl: string
+  imgAlt: string
+}
+
+const partnerItems: PartnersInfoItems[] = [
+  {
+    imgUrl: 'src/assets/images/allianz-copy-2.png',
+    imgAlt: 'allianz logo',
+  },
+  {
+    imgUrl: 'src/assets/images/creditshelf-copy-2.png',
+    imgAlt: 'creditshelf logo',
+  },
+  {
+    imgUrl: 'src/assets/images/usercentrics-copy-2.png',
+    imgAlt: 'usercentrics logo',
+  },
+  {
+    imgUrl: 'src/assets/images/laserhub.png',
+    imgAlt: 'laserhub logo',
+  },
+  {
+    imgUrl: 'src/assets/images/talixo-logo-black-copy-5.png',
+    imgAlt: 'talixo logo',
+  },
+];
+
+export default partnerItems;
