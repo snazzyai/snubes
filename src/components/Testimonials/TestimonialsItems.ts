39d6541882b5ddee7a2b0c70c07e32618308ce50
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2022-02-04 21:48:33 +0100

    renamed file

--- src/components/Testimonials/TestimonialsItems.ts
+++ src/components/Testimonials/TestimonialsItems.ts
@@ -6,7 +6,7 @@ interface TestimonialInfoItem {
   imgAlt: string
 }
 
-const testimonialItems: TestimonialInfoItem[] = [
+const testimonialsItems: TestimonialInfoItem[] = [
   {
     imgUrl: '/src/assets/images/talixo-logo-black-copy-5-2.png',
     quote: '"Finding a suitable outsourcing service provider through Snubes was very easy and our personal consultant helped us every step of the way. After a short time we had good offers on the table. An on-site visit confirmed the positive impression and we have now found a matching partner. Thank you for your great support, which has helped us a lot in the selection process."',
@@ -30,4 +30,4 @@ const testimonialItems: TestimonialInfoItem[] = [
   },
 ];
 
-export default testimonialItems;
+export default testimonialsItems;
