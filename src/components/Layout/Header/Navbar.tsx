f81e3b6b89b3f3f604944c469b530f4e8dc2a083
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2022-02-04 17:11:32 +0100

    changed src path

--- src/components/Layout/Header/Navbar.tsx
+++ src/components/Layout/Header/Navbar.tsx
@@ -4,7 +4,7 @@ import Nav from './Nav/Nav';
 const Navbar: React.FC = () => (
   <div className="Header">
     <div className="logo">
-      <img src="/src/assets/snubes-logo.png" alt="snubes logo" />
+      <img src="/src/assets/images/snubes-logo.png" alt="snubes logo" />
     </div>
     <Nav />
 
