c435a9e54f12c6ac42e0978bc749d4c247301dec
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2022-02-04 20:39:34 +0100

    imported css for slick

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import './App.scss';
 import Navbar from './components/Layout/Header/Navbar';
 import Body from './components/Layout/Body/Body';
