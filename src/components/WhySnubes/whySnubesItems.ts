f344ed49c6b8379d149fae1c6a09decbb4a609ea
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2022-02-04 17:10:02 +0100

    added data item for component WhySnubes

---
+++ src/components/WhySnubes/whySnubesItems.ts
@@ -0,0 +1,21 @@
+interface WhySnubesInfoItem {
+  heading: string
+  text: string
+}
+
+const whySnubesItems: WhySnubesInfoItem[] = [
+  {
+    heading: 'Trusted Know-How',
+    text: 'We have more than 20 years of experience and continuosly check call centers fortheir quality and capabilities. Customers like Allianz trust Snubes industry knowledge',
+  },
+  {
+    heading: 'Time and cost savings ',
+    text: 'With Snubes you can easily compare prices, quality and availability, and find more potential suppliers. Reach your goal faster and save resources',
+  },
+  {
+    heading: 'Satisfaction guaranted',
+    text: 'We offer a validated data-based approach to procurement, rather than awarding contracts only based on the lowest price, an existing relationship or "gut feeling"',
+  },
+
+];
+export default whySnubesItems;
