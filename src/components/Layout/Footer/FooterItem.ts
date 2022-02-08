d161a2b2b252f66b611c8f3c758b6e14025eadff
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2022-02-08 10:25:12 +0100

    added footer data items

---
+++ src/components/Layout/Footer/FooterItem.ts
@@ -0,0 +1,47 @@
+interface FooterInfoItem {
+  title: string
+  url: string
+}
+
+const footerInfoItem: Array<Array<FooterInfoItem>> = [
+  [
+    {
+      title: 'About Us',
+      url: '#',
+    },
+    {
+      title: 'Become a Partner',
+      url: '#',
+    },
+    {
+      title: 'FAQ',
+      url: '#',
+    },
+  ],
+  [
+    {
+      title: 'Imprint',
+      url: '#',
+    },
+    {
+      title: 'Terms & Conditions',
+      url: 'Privacy Policy',
+    },
+    {
+      title: 'support@snubes.com',
+      url: '#',
+    },
+  ],
+  [
+    {
+      title: '+49 (0) 305 5645327',
+      url: '#',
+    },
+    {
+      title: 'Become a Partner',
+      url: '#',
+    },
+  ],
+];
+
+export default footerInfoItem;
