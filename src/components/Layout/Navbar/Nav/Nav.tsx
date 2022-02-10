e18c12edc42b906d1ea76a533769cccf7593e856
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-02-10 04:24:53 +0100

    updated Nav styles and content

---
+++ src/components/Layout/Navbar/Nav/Nav.tsx
@@ -0,0 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import useStore from '../../../../store/store';
+
+import './Nav.scss';
+import menuItems from './menuItems';
+
+const Nav: React.FC = () => {
+  const { isNavToggled, navToggle } = useStore();
+  const handleBurger = () => {
+    navToggle();
+    console.log(isNavToggled);
+  };
+  return (
+    <>
+      <ul className="nav-menu">
+        {menuItems.map((item) => (
+          <>
+            <li className={item.className} key={item.title}>
+              {item.title}
+            </li>
+            { item.title.includes('EUR') || item.title.includes('EN') ? <span><FontAwesomeIcon icon={faAngleDown} /></span> : null }
+          </>
+        ))}
+      </ul>
+      <div className="nav-menu__burger">
+        { isNavToggled ? <FontAwesomeIcon icon={faXmark} size="2x" onClick={handleBurger} color="grey" /> : <FontAwesomeIcon icon={faBars} size="2x" onClick={handleBurger} color="grey" />}
+      </div>
+    </>
+  );
+};
+
+export default Nav;
