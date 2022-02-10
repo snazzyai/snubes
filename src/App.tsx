9c74b83d38399622729d226f48ced38545ac1fe0
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-02-10 04:12:53 +0100

    path change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import './App.scss';
-import Navbar from './components/Layout/Header/Navbar';
+import Navbar from './components/Layout/Navbar/Navbar';
 import Body from './components/Layout/Body/Body';
 import Footer from './components/Layout/Footer/Footer';
 
