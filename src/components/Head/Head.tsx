966a4aee749bd32a5577d3be36ae533f15e5103c
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-02-11 21:02:44 +0100

    updated component

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,12 +1,21 @@
 import './Head.scss';
 import HeaderInfo from '../HeaderInfo/HeaderInfo';
 import Form from '../Form/Form';
+import { useFormStore } from '../../store/store';
+import Submission from '../Submission/Submission';
 
-const Head: React.FC = () => (
-  <div className="head">
-    <Form />
-    <HeaderInfo />
-  </div>
-);
+const Head: React.FC = () => {
+  const {
+    formIsSubmitted,
+  } = useFormStore();
+  return (
+    <div
+      className="head"
+    >
+      {formIsSubmitted ? <Submission /> : <Form />}
+      <HeaderInfo />
+    </div>
+  );
+};
 
 export default Head;
