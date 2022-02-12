5e396c85c8f917c5bdb4219cba7ad4959ba43e32
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-02-12 01:05:20 +0100

    added prop to validation

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -6,10 +6,10 @@ import countryData from '../../data/countrycode';
 import useGeoLocationFinder from '../../hooks/useGeoLocation';
 
 const schema = yup.object().shape({
-  company: yup.string().matches(/^[A-Za-z]+$/).max(80),
-  name: yup.string().max(50),
-  phone: yup.string().matches(/^[0-9]+$/),
-  email: yup.string().email(),
+  company: yup.string().required().matches(/^[A-Za-z]+$/).max(80),
+  name: yup.string().required().max(50),
+  phone: yup.string().required().matches(/^[0-9]+$/),
+  email: yup.string().required().email(),
 
 });
 
@@ -19,6 +19,7 @@ const FormInput: React.FC = () => {
 
   return (
     <div className="form-input">
+
       <div className="form-input__label-view">
         <label className="margin-left" htmlFor="company">Company</label>
         <label htmlFor="fullName">Name</label>
@@ -43,12 +44,11 @@ const FormInput: React.FC = () => {
         }) => (
           <form className="form-input__form" onSubmit={handleSubmit}>
             <div className="form-input__item">
-
               <input
                 id="company"
                 name="company"
                 type="text"
-                style={errors.company && { border: '1px solid red' }}
+                style={errors.company ? { border: '1px solid red' } : { border: '1px solid #0099EE' }}
                 className="form-input__item__input form-input__item__input--add-margin"
                 onChange={(event) => {
                   handleChange(event);
@@ -64,7 +64,7 @@ const FormInput: React.FC = () => {
                 id="name"
                 name="name"
                 type="text"
-                style={errors.name && { border: '1px solid red' }}
+                style={errors.company ? { border: '1px solid red' } : { border: '1px solid #0099EE' }}
                 className="form-input__item__input"
                 onChange={(event) => {
                   handleChange(event);
@@ -75,7 +75,7 @@ const FormInput: React.FC = () => {
               />
             </div>
 
-            <div className="form-input__item">
+            <div className="form-input__item phone__view">
               <span>
                 <select
                   className="form-input__item__select"
@@ -86,33 +86,36 @@ const FormInput: React.FC = () => {
                 }
                 >
                   { code ? <option value="code">{code}</option> : countryData.map((data) => (
-                    <option value={data.dial_code}>
+                    <option value={data.dial_code} key={data.code}>
                       {data.dial_code}
                     </option>
                   ))}
 
                 </select>
               </span>
-              <input
-                id="phone"
-                name="phone"
-                type="phone"
-                style={errors.phone && { border: '1px solid red' }}
-                className="form-input__item__input phone__input"
-                onChange={(event) => {
-                  handleChange(event);
-                  storeItems.updatePhone(event.target.value);
-                }}
-                value={values.phone}
-                placeholder=""
-              />
+              <span>
+                <input
+                  id="phone"
+                  name="phone"
+                  type="phone"
+                  style={errors.company ? { border: '1px solid red' } : { border: '1px solid #0099EE' }}
+                  className="form-input__item__input phone__input"
+                  onChange={(event) => {
+                    handleChange(event);
+                    storeItems.updatePhone(event.target.value);
+                  }}
+                  value={values.phone}
+                  placeholder=""
+                />
+              </span>
+
             </div>
             <div className="form-input__item">
               <input
                 id="email"
                 name="email"
                 type="email"
-                style={errors.email && { border: '1px solid red' }}
+                style={errors.company ? { border: '1px solid red' } : { border: '1px solid #0099EE' }}
                 className="form-input__item__input"
                 onChange={(event) => {
                   handleChange(event);
