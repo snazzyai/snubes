d9b2c66afc2b6dbb275b8691605bd2bc0106247b
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-02-12 01:08:19 +0100

    added lazyloading

--- src/components/Offerings/Offerings.tsx
+++ src/components/Offerings/Offerings.tsx
@@ -1,15 +1,19 @@
 import './Offerings.scss';
+import LazyLoad from 'react-lazyload';
 import offeringsItem from './offeringsItem';
 
 const Offerings: React.FC = () => (
   <div className="offerings">
     {
           offeringsItem.map((offering) => (
-            <div className="offerings-item" key={offering.heading}>
-              <img src={offering.imgUrl} alt={offering.imgAlt} />
-              <p className="offerings-item__heading">{offering.heading}</p>
-              <p className="offerings-item__text">{offering.text}</p>
-            </div>
+            <LazyLoad key={offering.imgAlt}>
+              <div className="offerings-item">
+                <img src={offering.imgUrl} alt={offering.imgAlt} />
+                <p className="offerings-item__heading">{offering.heading}</p>
+                <p className="offerings-item__text">{offering.text}</p>
+              </div>
+            </LazyLoad>
+
           ))
       }
   </div>
