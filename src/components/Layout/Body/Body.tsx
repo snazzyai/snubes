ef1e7b36ae18cfb45df9f5a1d4536ca0225dbe1c
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-02-12 01:03:39 +0100

    added lazyloading

--- src/components/Layout/Body/Body.tsx
+++ src/components/Layout/Body/Body.tsx
@@ -1,3 +1,4 @@
+import LazyLoad from 'react-lazyload';
 import Head from '../../Head/Head';
 import Offerings from '../../Offerings/Offerings';
 import WhySnubes from '../../WhySnubes/WhySnubes';
@@ -6,12 +7,21 @@ import Partners from '../../Partners/Partners';
 
 const Body: React.FC = () => (
   <div>
-    <Head />
-    <Offerings />
-    <WhySnubes />
-    <Testimonials />
-    <Partners />
+    <LazyLoad height={200} once>
+      <Head />
+    </LazyLoad>
+    <LazyLoad height={200} once>
+      <Offerings />
+    </LazyLoad>
+    <LazyLoad height={200} once>
+      <WhySnubes />
+    </LazyLoad>
+    <LazyLoad height={200} once>
+      <Testimonials />
+    </LazyLoad>
+    <LazyLoad height={200} once>
+      <Partners />
+    </LazyLoad>
   </div>
 );
-
 export default Body;
