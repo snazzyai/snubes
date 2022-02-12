e37a7339f0452a4068aa308a0411925ff4d7de0a
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-02-12 01:07:37 +0100

    added react fragment

--- src/components/Layout/Navbar/Nav/Nav.tsx
+++ src/components/Layout/Navbar/Nav/Nav.tsx
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Fragment } from 'react';
 import { faBars, faXmark, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { useToggleStore } from '../../../../store/store';
 
@@ -9,18 +10,17 @@ const Nav: React.FC = () => {
   const { isNavToggled, navToggle } = useToggleStore();
   const handleBurger = () => {
     navToggle();
-    console.log(isNavToggled);
   };
   return (
     <>
       <ul className="nav-menu">
         {menuItems.map((item) => (
-          <>
-            <li className={item.className} key={item.title}>
+          <Fragment key={item.title}>
+            <li className={item.className}>
               {item.title}
             </li>
             { item.title.includes('EUR') || item.title.includes('EN') ? <span><FontAwesomeIcon icon={faAngleDown} /></span> : null }
-          </>
+          </Fragment>
         ))}
       </ul>
       <div className="nav-menu__burger">
