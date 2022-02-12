cf7948f158a59ee28e1e9b4d01bd177bf9ef36a8
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-02-12 01:06:35 +0100

    removed lazy function

--- src/components/HeaderInfo/HeaderInfo.tsx
+++ src/components/HeaderInfo/HeaderInfo.tsx
@@ -1,7 +1,9 @@
 import './HeaderInfo.scss';
+// import { lazy } from 'react';
 import headerInfoItem from './headerInfoItem';
 
 const HeaderInfo: React.FC = () => (
+  // const headerInfoItem = lazy(() =>import('./headerInfoItem'))
   <div className="header-info">
     <p className="header-info__text">Welcome to Europes largest call center database</p>
     <div className="header-info--items">
